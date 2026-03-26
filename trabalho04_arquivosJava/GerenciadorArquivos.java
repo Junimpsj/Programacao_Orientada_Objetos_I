3247cd5d718e0c6ed0677380eaae0968bdbba718
Junimpsj <pjunior.celso@gmail.com>
2026-03-25 23:39:06 -0300

    criando arquivo trabalho04

---
+++ trabalho04_arquivosJava/GerenciadorArquivos.java
@@ -0,0 +1,14 @@
+package trabalho04_arquivosJava;
+import java.util.Scanner;
+
+public class GerenciadorArquivos {
+    
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        sc.close();
+
+    }
+
+}
